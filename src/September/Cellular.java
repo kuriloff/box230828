c5584e5c1cebd198f4e2758faa1b0730ed9e3dba
kuriloff <143285623+kuriloff@users.noreply.github.com>
2023-08-31 14:47:22 +0400

    1

--- src/September/Cellular.java
+++ src/September/Cellular.java
@@ -5,9 +5,14 @@ public class Cellular {
         Phone nokia = new Phone("1234567890","AMT-69", 3200);
         Phone samsung = new Phone("987654321", "VEF TA-68", 1200);
         Phone rigonda = new Phone("854619370","Jaskier-74", 800 );
+        Phone tesla = new Phone();
+        tesla.setModel("TA-72");
+        tesla.setNumber("92-184");
+        tesla.setWeight(750);
         System.out.printf("Apparatus model: %s, Serial number: %s, weight: %d \n", nokia.getModel(), nokia.getNumber(), nokia.getWeight());
         System.out.printf("Apparatus model: %s, Serial number: %s, weight: %d \n", samsung.getModel(), samsung.getNumber(), samsung.getWeight());
         System.out.printf("Apparatus model: %s, Serial number: %s, weight: %d \n", rigonda.getModel(), rigonda.getNumber(), rigonda.getWeight());
+        System.out.printf("Apparatus model: %s, Serial number: %s, weight: %d \n", tesla.getModel(), tesla.getNumber(), tesla.getWeight());
         nokia.receiveCall("Kremlin");
         System.out.println(nokia.getStatus());
         nokia.hangup();
