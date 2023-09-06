04db551048be902ac3b6a6c0129d6a4206cd9df0
kuriloff <143285623+kuriloff@users.noreply.github.com>
2023-09-06 07:23:47 +0400

    1

--- src/Sept05/SimpleGame.java
+++ src/Sept05/SimpleGame.java
@@ -9,18 +9,18 @@ public class SimpleGame {
         int j = ((int) (Math.random() * (MAX + 1))) + 1;
         int i = 0;
         Scanner scanner = new Scanner(System.in);
-        System.out.printf("Please, input integer number between 1 and %d or Zero to quit the game: \n", MAX);
+        System.out.printf("Please, input integer number between 1 and %d or Zero to quit the game \n>>", MAX);
         {
             while(true){
                 try {
                     i = scanner.nextInt();
-                    if (i == 0){System.out.println("It's pity, but hope to see you again.."); break;}
-                    else if(i < j){System.out.println("You're too modest, try to print some bigger number");}
-                    else if (i > j){System.out.println("Oh, your number is too big, try again");}
+                    if (i == 0){System.out.println("It's pity, but hope to see you again..\n"); break;}
+                    else if(i < j){System.out.print("You're too modest, try to print some bigger number\n>>");}
+                    else if (i > j){System.out.print("Oh, your number is too big, try again\n>>");}
                     else if(i == j){System.out.println("Yess, you were able to catch your lion in Sahara!"); break;}
                 } catch (InputMismatchException e) {
                     //e.printStackTrace();
-                    System.out.println("Wrong input, try again");
+                    System.out.print("Wrong input, try again\n>>");
                     scanner.next();
                 }
             }
