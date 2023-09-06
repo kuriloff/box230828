cf4d756a4b71a823f1d9797f03ea0f615295dc27
kuriloff <143285623+kuriloff@users.noreply.github.com>
2023-09-06 07:18:57 +0400

    1

--- src/Sept05/SimpleGame.java
+++ src/Sept05/SimpleGame.java
@@ -5,16 +5,15 @@ import java.util.Scanner;
 
 public class SimpleGame {
     public static void main(String[] args) {
-        final int MAX = 1000;
-        int j = 0;
-        j = ((int) (Math.random() * (MAX + 1))) + 1;
+        final int MAX = 100;
+        int j = ((int) (Math.random() * (MAX + 1))) + 1;
         int i = 0;
-        Scanner s = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         System.out.printf("Please, input integer number between 1 and %d or Zero to quit the game: \n", MAX);
         {
             while(true){
                 try {
-                    i = s.nextInt();
+                    i = scanner.nextInt();
                     if (i == 0){System.out.println("It's pity, but hope to see you again.."); break;}
                     else if(i < j){System.out.println("You're too modest, try to print some bigger number");}
                     else if (i > j){System.out.println("Oh, your number is too big, try again");}
@@ -22,7 +21,7 @@ public class SimpleGame {
                 } catch (InputMismatchException e) {
                     //e.printStackTrace();
                     System.out.println("Wrong input, try again");
-                    s.next();
+                    scanner.next();
                 }
             }
         }
